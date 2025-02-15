b691aa34189edbf884004932bb35758fe2614881
arielxct <campostula@gmail.com>
2025-02-15 18:17:52 -0300

    Agrego la funcionalidad agregarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listado_amigos = [];
+
+// ************(1) funcion agregar amigos ******************
+function agregarAmigo() {
+    const input = document.getElementById("amigo");
+    const nombre = input.value.trim().toUpperCase();
+
+    if (nombre === "") {
+        alert("Debe ingrear un nombre.");
+        return;
+    }
+    if (!isNaN(nombre)) {
+        alert("El nombre no puede ser un número.");
+        input.value = "";
+        return;
+    }
+    const regex = /^[A-Za-z\s]+$/; 
+    if (!regex.test(nombre)) {
+        alert("El nombre no puede contener símbolos especiales.");
+        input.value = "";
+        return;
+    }
+
+    listado_amigos.push(nombre);
+    // actualizarLista(); // funcion que actualizara la lista de amigos
+    input.value = "";
+}
